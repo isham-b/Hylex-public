4486a80bdb5a3d450e118902306c4d9986e3af96
isham-b <66917399+isham-b@users.noreply.github.com>
2020-06-25 14:39:10 -0400

    Added separate files for cogs/command help

---
+++ commands_help.py
@@ -0,0 +1,11 @@
+general_help = '''~ping - Displays the latency of Hylex Bot.
+               ~8ball <question> - Uses the magical 8ball to answer your question.
+            
+            '''
+
+mod_help = '''~clear <amount> - Clears a given amount of messages from the channel.
+            ~kick <user> - Kicks a mentioned user from the server.
+            ~ban <user> - Bans a mentioned user from the server.
+            ~unban <username#....> - Unbans a user given their Discord name and 4-digit discriminator. 
+            '''
+
