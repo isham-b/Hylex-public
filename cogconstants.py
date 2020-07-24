e7889f48ce7ea875c51ac1f3db13e93af20e2322
isham-b <66917399+isham-b@users.noreply.github.com>
2020-07-23 22:02:09 -0400

    Cleaned up Interface/Added LoL rank checker

---
+++ cogconstants.py
@@ -0,0 +1,39 @@
+GENERAL_RESPONSES = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.",
+                    "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.",
+                    "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+LOLPARSER_SERVERS = ['br', 'jp', 'euw', 'oce', 'lan', 'tr', 'www', 'na', 'eune',
+                    'las', 'ru']
+
+
+LOLPARSER_RANKICONS = {'Unranked': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/3/38/Season_2019_-_Unranked.png/revision/latest?cb=20190908074432', 
+                       'Provisional': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5e/Season_2019_-_Provisional.png/revision/latest?cb=20190908074433', 
+                       'Iron 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/0/03/Season_2019_-_Iron_1.png/revision/latest?cb=20181229234926',
+                       'Iron 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5f/Season_2019_-_Iron_2.png/revision/latest?cb=20181229234927', 
+                       'Iron 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/9/95/Season_2019_-_Iron_3.png/revision/latest?cb=20181229234927', 
+                       'Iron 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Iron_4.png/revision/latest?cb=20181229234928', 
+                       'Bronze 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/f/f4/Season_2019_-_Bronze_1.png/revision/latest?cb=20181229234910', 
+                       'Bronze 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/a/ac/Season_2019_-_Bronze_2.png/revision/latest?cb=20181229234911', 
+                       'Bronze 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/8/81/Season_2019_-_Bronze_3.png/revision/latest?cb=20181229234912', 
+                       'Bronze 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5a/Season_2019_-_Bronze_4.png/revision/latest?cb=20181229234913', 
+                       'Silver 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Silver_1.png/revision/latest?cb=20181229234936', 
+                       'Silver 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/5/56/Season_2019_-_Silver_2.png/revision/latest?cb=20181229234936',
+                       'Silver 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/1/19/Season_2019_-_Silver_3.png/revision/latest?cb=20181229234937',
+                       'Silver 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/5/52/Season_2019_-_Silver_4.png/revision/latest?cb=20181229234938',
+                       'Gold 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/9/96/Season_2019_-_Gold_1.png/revision/latest?cb=20181229234920', 
+                       'Gold 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/8/8a/Season_2019_-_Gold_2.png/revision/latest?cb=20181229234921',
+                       'Gold 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/a/a6/Season_2019_-_Gold_3.png/revision/latest?cb=20181229234921',
+                       'Gold 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/c/cc/Season_2019_-_Gold_4.png/revision/latest?cb=20181229234922',
+                       'Platinum 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/7/74/Season_2019_-_Platinum_1.png/revision/latest?cb=20181229234932', 
+                       'Platinum 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/a/a3/Season_2019_-_Platinum_2.png/revision/latest?cb=20181229234933', 
+                       'Platinum 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/2/2b/Season_2019_-_Platinum_3.png/revision/latest?cb=20181229234934', 
+                       'Platinum 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/a/ac/Season_2019_-_Platinum_4.png/revision/latest?cb=20181229234934', 
+                       'Diamond 1': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/9/91/Season_2019_-_Diamond_1.png/revision/latest?cb=20181229234917', 
+                       'Diamond 2': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Diamond_2.png/revision/latest?cb=20181229234918', 
+                       'Diamond 3': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/d/dc/Season_2019_-_Diamond_3.png/revision/latest?cb=20181229234918', 
+                       'Diamond 4': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/e/ec/Season_2019_-_Diamond_4.png/revision/latest?cb=20181229234919', 
+                       'Master': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/0/01/Season_2019_-_Master_4.png/revision/latest?cb=20181229234931', 
+                       'Grandmaster': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/4/42/Season_2019_-_Grandmaster_4.png/revision/latest?cb=20181229234925', 
+                       'Challenger': 'https://vignette.wikia.nocookie.net/leagueoflegends/images/e/e3/Season_2019_-_Challenger_4.png/revision/latest?cb=20181229234916'
+                       
+                       }
